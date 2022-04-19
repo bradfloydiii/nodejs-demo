f62ada6032457d62eea399d539e89e052ccc39fa
bradfloydiii <bfloydiii@gmail.com>
2022-04-18 20:50:05 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+const hostname = 'localhost';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  console.log(port);
+  res.end('Hello World test');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`server ready on http://${hostname}:${port}/`);
+});
